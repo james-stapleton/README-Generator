ea85068ecf51bac913ca0a505b34bdffc8196319
“jms” <jstapleton536@gmail.com>
2022-09-08 13:27:30 -0400

    Prompt function basic test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -7,7 +8,32 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    // title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of your app?',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Describe your app: ',
+                },
+        ])
+        .then((data) => {
+            const filename = `${data.title.toLowerCase().split(' ').join('')}.md`;
+            const stringMD = 
+            `## Title
+            ${data.title}
+            ## Description
+            ${data.description}`;
+            fs.writeFile(filename, stringMD, (err) => err ? console.log(err) : console.log('Success!'));
+        })
+}
 
 // Function call to initialize app
 init();
